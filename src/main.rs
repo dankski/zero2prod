7827182c8cbf0f26149ae9357693f72ad2e5899a
Giacomo Amoro <giacomo@amoroso.me>
2022-11-13 17:11:54 +0100

    Ground work integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,9 @@
-use actix_web::{
-    web, 
-    App, 
-    HttpRequest,
-    HttpResponse,
-    HttpServer, 
-    Responder
-};
+//! main.rs
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
+use zero2prod::run;
 
-    format!("Hello {}!", &name)
-}
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
-
-
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
-
+    run().await
 }
+
