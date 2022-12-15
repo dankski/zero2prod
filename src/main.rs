1828cc3fa4414562d6c84067cb970d37b5365077
Giacomo Amoroso <giacomo@amoroso.me>
2022-12-15 19:36:38 +0100

    Test Comment

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-
+    // Added this comment
     run(listener)?.await
 }
 
