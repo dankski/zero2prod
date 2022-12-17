e21594c9be51f44729828d3b279493e492137829
Giacomo Amoroso <giacomo@amoroso.me>
2022-12-17 21:34:41 +0100

    Added pinging check

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -16,4 +16,16 @@ docker run \
     -d postgres \
     postgres -N 1000
 
+# Keep pinging Postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "Postgres is still unavaible - sleeping"
+    sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
 
