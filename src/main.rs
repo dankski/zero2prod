4b299fe5fc7fab90e1a4d1ab4e7a1cb5cf0d30b4
Giacomo Amoroso <giacomo@amoroso.me>
2023-04-08 10:09:23 +0200

    4.3.3 The Facade Pattern [done]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 //! main.rs
+use env_logger::Env;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use sqlx::PgPool;
@@ -7,6 +8,9 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(
         &configuration.database.connection_string()
